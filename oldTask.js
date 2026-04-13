b5b921f7b2a4446ba354d2567a970daab5c7b693
Daniel- new <bassygit@gmail.com>
2026-04-13 22:11:19 +0100

    my old assignment

---
+++ oldTask.js
@@ -0,0 +1,71 @@
+///PRACTICE QUESTIONS//////////
+//no 1
+// const numbers = [1, 2, 3, 4, 5, 6,]
+// function getevennumbers(number) {
+//             return number % 2 === 0
+// }
+// const evennumbers = numbers.filter(getevennumbers)
+// console.log(getevennumbers(evennumbers)
+
+//no 2
+// const capitalizeword = (name) => {
+//             return name.toUpperCase()
+// }
+// console.log(capitalizeword("javascript"))
+
+//no 3
+// const arrayOfNumbers = [2, 4, 6]
+// function sumNumbers(sum, i) {
+//             return sum += i
+// }
+// const calculatesum = arrayOfNumbers.reduce(sumNumbers)
+// console.log(calculatesum)
+
+//no 4
+// const arrOfNumbers = [1, 2, 3]
+// const cubeNumbers = (number) => {
+//             return (number * number * number)
+// }
+// const multiplyNo = arrOfNumbers.map(cubeNumbers)
+// console.log(multiplyNo)
+
+//no 5
+// const letters = "javascript"
+// function checkWord() {
+//             return letters.includes("j")
+// }
+// console.log(checkWord())
+
+//no 6
+// const convertToNumber = (wholeNumber) => {
+//             return wholeNumber = parseInt("45")
+// }
+// console.log(convertToNumber())
+
+//no 7
+// const names = ["ada", "chinedu", "mary", "emeka"]
+// function NamesLongerThanFour() {
+//             return names.filter((names) => names.length > 4)
+// }
+
+// console.log(NamesLongerThanFour(names))
+
+
+// no 8
+// const forReverseText = ( word) => {
+// return word.split('').reverse().join('')
+// }
+// console.log(forReverseText('hello'))
+
+//no 9
+// function forRoundNumbers (numbers){
+//   return numbers.map(num => Math.round(num));
+// }
+// const Numbers = [2.3, 4.8, 5.1]
+// console.log(forRoundNumbers(Numbers))
+
+//no 10
+// const getPassedScores = (scores) => {
+//   return scores.filter(i => i >= 50)
+// }
+// console.log(getPassedScores([30,55,70,40,90]))
